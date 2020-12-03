1ba81b13bdab1ac495c25369516ddea8eb4cdfe4
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-12-03 16:38:18 -0600

    added error.vue page. Will polish it in a later branch

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- it's as simple as adding an error.vue page in the layouts folder -->
+    <h4>There was an erro</h4>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
