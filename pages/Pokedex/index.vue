649a79973b4d226910867b2935b84e814fa96a68
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-12-07 00:05:14 -0600

    todo: compare this to the most recent branch (branch 6, to make sure everything is the same. I had issues merging so Il me faut verifier. Hopefully I won't have any merging issues moving forward

--- pages/Pokedex/index.vue
+++ pages/Pokedex/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="pokecard-container">
-    <Pokecard v-for="(currentPokemon, index) in pokemonList" v-bind:key="index" v-bind:pokemonObj="currentPokemon" />
+    <!-- NEW: The pokeball spins while we're fetching Pokemon from the API -->
+    <Pokeball v-if="loading" class="lg spinnin" />
+    <Pokecard v-else v-for="(currentPokemon, index) in pokemonList" v-bind:key="index" v-bind:pokemonObj="currentPokemon" />
   </div>
 </template>
 
@@ -8,7 +10,6 @@
 import axios from "@nuxtjs/axios";
 import Pokeball from "~/components/shared/Pokeball";
 import Pokecard from "~/components/Pokecard";
-
 export default {
   name: "Pokedex",
   components: {
@@ -18,25 +19,31 @@ export default {
   data() {
     return {
       pokemonList: [],
+      //NEW:
+      loading: false,
     };
   },
   methods: {
     async promiseFunc(iterator) {
-      const URL = "https://pokeapi.co/api/v2/pokemon";
       try {
-        const response = await this.$axios.get(`${URL}/${iterator}`);
-        const data = response.data;
-        return data;
+        const response = await this.$axios.$get(
+          `https://pokeapi.co/api/v2/pokemon/${iterator}`
+        );
+        return response;
       } catch (error) {
         console.log(error);
       }
     },
   },
   async created() {
+    // NEW: loading is true as we're fetching the pokemon
+    this.loading = true;
     for (let i = 1; i <= 150; i++) {
       this.pokemonList.push(await this.promiseFunc(i));
     }
     console.log(this.pokemonList);
+    //NOTE: after we've fethe all the pokemon, then we set loading to false, which will make the loader stop and the pokecards will render
+    this.loading = false;
   },
 };
 </script>
@@ -46,22 +53,17 @@ export default {
   position: relative;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(25rem, 27.5rem));
-  // So I don't know what I like better: 27.5rem for the largest possible unit of 1fr. Je vais decider plus tard
   gap: 2rem;
   justify-content: center;
-
   margin: 0 1.5rem;
 }
-
 .lg {
   width: 50rem;
   height: 50rem;
 }
-
 .spinnin {
   animation: spin 1.25s linear infinite; // linear is a value for what property again? mmmm.
 }
-
 @keyframes spin {
   from {
     transform: rotate(0);
@@ -70,4 +72,4 @@ export default {
     transform: rotate(360deg);
   }
 }
-</style>  
+</style>
